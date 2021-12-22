9ead0f8fbb976a651321d3c8bfe524dc70832e05
monirulislamriaj <riaj1132@gmail.com>
2021-12-22 17:30:25 +0600

    added:sing up & login page upded:responsive

--- src/js/script.js
+++ src/js/script.js
@@ -119,7 +119,17 @@ $(function () {
 
   // counter-up
   $(".counter").counterUp({});
-
+  
+ //Show password
+ $( ".viewPassword" ).click(function() {
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
   // Scroll spy style start
   $(".nav-link").click(function () {
     $(".nav-link").removeClass("active");
@@ -243,7 +253,44 @@ $(function () {
  }
  //pricing-two section  filter-price end
   
+ 
+
+// COPY TO CLIPBOARD
+
+function copyToClipboard(text, el) {
+  var copyTest = document.queryCommandSupported('copy');
+  var elOriginalText = el.attr('data-original-title');
+  if (copyTest === true) {
+    var copyTextArea = document.createElement("textarea");
+    copyTextArea.value = text;
+    document.body.appendChild(copyTextArea);
+    copyTextArea.select();
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'Copied!' : 'Whoops, not copied!';
+      el.attr('data-original-title', msg).tooltip('show');
+    } catch (err) {
+      console.log('Oops, unable to copy');
+    }
+    document.body.removeChild(copyTextArea);
+    el.attr('data-original-title', elOriginalText);
+  }
+  else {
+    // Fallback if browser doesn't support .execCommand('copy')
+    window.prompt("Copy to clipboard: Ctrl+C or Command+C, Enter", text);
+  }
+}
+  $(document).ready(function () {
+    $('.js-tooltip').tooltip();
+  $('.js-copy').click(function() {
+    var text = $(this).attr('data-copy');
+    var el = $(this);
+    copyToClipboard(text, el);
+  });
+});
+  
   // Add active class to the current accordionExample
+
   var header = document.getElementById("accordionExample");
   var btns = header.getElementsByClassName("accordion-item");
   for (var i = 0; i < btns.length; i++) {
